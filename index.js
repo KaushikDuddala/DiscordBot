0bb89d51d37421fce7b81e64457eed59b1719a0e
KaushikDuddala <kaushik.duddala@gmail.com>
2024-06-24 17:16:44 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(token);
